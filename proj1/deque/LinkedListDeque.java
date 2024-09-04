d9e44812c4648f6ae47ad64d349e7013ebfda5bc
Junjie Zhou <menging1122@gmail.com>
2024-09-04 16:57:57 +0800

    refacor my code of linedlistdeque and rewrite the arrayDeque

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,6 +1,8 @@
 package deque;
+import java.util.Iterator;
 
-public class LinkedListDeque implements Deque<T>{
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T>{
+
     public static class Node<T> {
         Node<T> prev;
         T item;
@@ -24,13 +26,36 @@ public class LinkedListDeque implements Deque<T>{
         int size = 0;
     }
 
+    public LinkedListDeque(T item) {
+        sentinel.next = new Node<T>(item, sentinel, sentinel);
+        sentinel.prev = sentinel.next;
+        int size = 1;
+    }
+
     @Override
     public void addFirst(T item) {
-
+        size++;
+        if (sentinel.next == sentinel) {
+            sentinel.next = new Node<T>(item, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+        } else {
+            Node<T> p = new Node<T>(item, sentinel, sentinel.next);
+            sentinel.next.prev = p;
+            sentinel.next = p;
+        }
     }
 
     @Override
     public void addLast(T item) {
+        size++;
+        if (sentinel.next == sentinel) {
+            sentinel.next = new Node<T>(item, sentinel, sentinel);
+            sentinel.prev = sentinel.next;
+        } else {
+            Node<T> p = new Node<T>(item, sentinel.prev, sentinel);
+            sentinel.prev.next = p;
+            sentinel.prev = p;
+        }
 
     }
 
@@ -41,21 +66,93 @@ public class LinkedListDeque implements Deque<T>{
 
     @Override
     public void printDeque() {
+        Node<T> p = sentinel;
+        for (int i = 0; i < size; i++) {
+            System.out.print(p.next.item);
+            p = p.next;
 
+        }
+        System.out.println();
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+
+        T removeItem = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return removeItem;
+
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+
+        T removeItem = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -= 1;
+        return removeItem;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        Node<T> p = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            p = p.next;
+        }
+
+        return p.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >-= size) {
+            return null;
+        }
+
+        return helperRecursive(sentinel.next, index);
+
+    }
+
+    public T helperRecursive(Node<T> p, int index) {
+        if (index == 0) {
+            return p.item;
+        }
+        return helperRecursive(p.next, index);
+    }
+
+    public Iterator<T> iterator() {
+        return new LinkedListDequeIterator();
+    }
+
+    private class LinkedListDequeIterator implements Iterator<T>{
+        private Node<T> p;
+
+        public LinkedListDequeIterator() {
+            p = sentinel.next;
+        }
+
+
+        public boolean hasNext() {
+            return !(p == sentinel);
+        }
+
+
+        public T next() {
+            T item = p.item;
+            p = p.next;
+            return item;
+        }
+
     }
 }
